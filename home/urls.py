5b0fd355e0faef16b35f96d2fc24bf11b8ef3902
Veena Dharshini <veenadharshini.bs@wakefit.co>
2025-02-04 17:56:46 +0530

    blog module

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from home.views import BlogView, PublicBlogView
+
+urlpatterns = [
+    path('', PublicBlogView.as_view()),
+    path('blog/', BlogView.as_view())
+]
